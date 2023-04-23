b4c6ff125a1cf15ece7bca161c35923bdf4b8118
David Quenault <davidquenault@Davids-MacBook-Air.local>
2023-04-24 11:39:11 +1200

    new demo app

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = '39fbe7343b9e42452d93ead364d9d95012816803a567274696ceaec8088b7f1a5f495c7952ee1490ee556f215d1d45385d95dbe23b44badf05a862a2b63528ef'
